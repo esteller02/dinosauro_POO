b38a576e9da216ff7590c990777b597139cd9b36
Esteller Silva <esteller03@gmail.com>
2023-11-14 15:34:18 -0400

    juego terminado

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-import { Suelo, Juego } from "./clases.js";
+import { Suelo, Juego, Dinosaurio } from "./clases.js";
 //VARIABLES
 
 let tiempo_ES = new Date();
 let deltaTime_ES = 0;
 
+let gravedad_ES = 2500;
+
 let suelo_ES = new Suelo(document.getElementById("suelo"), 0, 22);
 let contenedor_ES = document.getElementById("contenedor");
 let velEscenario_ES = 1280/3;
 let gameVel_ES = 1;
+
+let tiempoHastaObstaculo = 2;
+let tiempoObstaculoMin = 0.7;
+let tiempoObstaculoMax = 1.8;
+
+
+
+let obstaculoPosY = 16;
+
+let obstaculos = [];
+let puntos = 0;
+
+let juego_ES = new Juego(tiempoHastaObstaculo, tiempoObstaculoMax, tiempoObstaculoMin, obstaculos, 0);
+
+let dinosaurio_ES = new Dinosaurio(42, 22, false, 900, 0, document.getElementById("dino"));
+
+
+
+
 
 
 
@@ -21,7 +42,7 @@ if(document.readyState === "complete" || document.readyState === "interactive"){
 //FUNCIONES
 function Init() {
     tiempo_ES = new Date();
-    //start();
+    start();
     loop();
 }
 
@@ -35,9 +56,31 @@ function loop() {
 function update()
 {
     //console.log("actualizando");
+
+    if(juego_ES.estado == false) return
     
     suelo_ES.MoverSuelo(velEscenario_ES, deltaTime_ES, gameVel_ES, contenedor_ES);
+
+    dinosaurio_ES.mover(deltaTime_ES, suelo_ES.sueloY);
+  
+    juego_ES.decidirCrearObstaculo(deltaTime_ES, gameVel_ES, contenedor_ES);
+
+    juego_ES.moverObstaculos(velEscenario_ES, deltaTime_ES, gameVel_ES);
+
+    juego_ES.detectarColision(dinosaurio_ES);
+    
+
+    dinosaurio_ES.velY -= gravedad_ES * deltaTime_ES
 };
+
+function start()
+{
+    
+    document.addEventListener("keydown",(e)=>{
+        
+        dinosaurio_ES.saltar(suelo_ES.sueloY, e);
+    })
+}
 
 
 
--- js/clases.js
+++ js/clases.js
@@ -10,9 +10,9 @@ export class Suelo
 
     MoverSuelo(velEscenario, deltaTime, gameVel, contenedor) 
     {
-        console.log(this.sueloX);
+        //console.log(this.sueloX);
         this.sueloX += Juego.CalcularDesplazamiento(velEscenario, deltaTime, gameVel);
-        console.log(this.sueloX);
+        //console.log(this.sueloX);
         this.suelo.style.left = -(this.sueloX % contenedor.clientWidth) + "px";
     }
 
@@ -22,15 +22,199 @@ export class Suelo
 
 export class Juego
 {
-    /*constructor ()
-    {
-        this.velEscenario = velEscenario,
-        this.deltaTime = deltaTime, 
-        this.gameVel = gameVel
-
-    }*/
+   constructor(tiempoHastaObstaculo, tiempoObstaculoMax, tiempoObstaculoMin, obstaculos, puntos, estado)
+   {
+        this.tiempoHastaObstaculo = tiempoHastaObstaculo,
+        this.tiempoObstaculoMax = tiempoObstaculoMax,
+        this.tiempoObstaculoMin = tiempoObstaculoMin,
+        this.obstaculos = obstaculos,
+        this.puntos = puntos,
+        this.estado = true
+   }
     static CalcularDesplazamiento(velEscenario, deltaTime, gameVel)
     {
         return velEscenario * deltaTime * gameVel;
+    }
+
+     decidirCrearObstaculo(deltaTime, gameVel, contenedor)
+    {
+        
+        this.tiempoHastaObstaculo -= deltaTime;
+
+        //console.log(this.tiempoHastaObstaculo);
+
+        if(this.tiempoHastaObstaculo <= 0)
+        {
+            console.log("entre")
+            this.crearObstaculo(gameVel, contenedor);
+        }   
+    }
+
+    crearObstaculo(gameVel, contenedor)
+    {
+        let obstaculoDiv = document.createElement("div");
+        
+        let obstaculo = new Obstaculo(contenedor.clientWidth, 16, obstaculoDiv);
+        obstaculo.obstaculoDiv.classList.add("cactus");
+
+        contenedor.appendChild(obstaculo.obstaculoDiv);
+
+    
+        //console.log(obstaculo, "soy el obstaculo creado");
+
+        obstaculo.obstaculoDiv.style.left = obstaculo.posX + "px";
+        
+
+        this.obstaculos.push(obstaculo);
+
+        
+
+        
+
+        this.tiempoHastaObstaculo = this.tiempoObstaculoMin + Math.random() * (this.tiempoObstaculoMax - this.tiempoObstaculoMin)/gameVel
+
+    }
+
+    moverObstaculos(velEscenario, deltaTime, gameVel)
+    {
+
+        
+        for (let i = this.obstaculos.length - 1; i >= 0; i--) 
+        {
+                
+                if(this.obstaculos[i].posX < -this.obstaculos[i].posX)
+                {
+                    this.obstaculos[i].obstaculoDiv.remove();
+    
+                    this.obstaculos.splice(i, 1);
+
+                    this.actualizarPuntos();
+                }    
+    
+                else
+                {
+                    this.obstaculos[i].posX -= Juego.CalcularDesplazamiento(velEscenario, deltaTime, gameVel);
+    
+                    this.obstaculos[i].obstaculoDiv.style.left = this.obstaculos[i].posX + "px";
+                }
+                
+        }
+    }
+
+    actualizarPuntos()
+    {
+        this.puntos++;
+        document.getElementById("score").innerText = this.puntos;
+    }
+
+
+    detectarColision(dinosaurio)
+    {
+        for (let i = 0;  i < this.obstaculos.length; i++) 
+        {
+            if(this.obstaculos[i].posX > dinosaurio.posX + dinosaurio.dinoDiv.clientWidth)
+            {
+                //EVADE
+                break;
+            }
+            
+            else
+            {
+                if(this.isCollision(dinosaurio.dinoDiv, this.obstaculos[i].obstaculoDiv, 10, 30, 15, 20))
+                {
+                    this.gameOver(dinosaurio);
+                }
+            }
+        }
+    }
+
+    isCollision(a, b,  paddingTop, paddingRight, paddingBottom, paddingLeft)
+    {
+        let aRect = a.getBoundingClientRect();
+        let bRect = b.getBoundingClientRect();
+    
+        return !(
+            ((aRect.top + aRect.height - paddingBottom) < (bRect.top)) ||
+            (aRect.top + paddingTop > (bRect.top + bRect.height)) ||
+            ((aRect.left + aRect.width - paddingRight) < bRect.left) ||
+            (aRect.left + paddingLeft > (bRect.left + bRect.width))
+        );
+    }
+
+    gameOver(dinosaurio)
+    {
+        dinosaurio.estrellarse();
+        this.estado = false;
+        document.getElementById("gameOver").style.display = "block";
+    }
+}
+
+
+export class Dinosaurio
+{
+    constructor(posX, posY, saltando, impulso, velY, dinoDiv)
+    {
+        this.posX = posX,
+        this.posY = posY,
+        this.saltando = saltando,
+        this.impulso = impulso,
+        this.velY = velY,
+        this.dinoDiv = dinoDiv
+    }
+
+    saltar(sueloY, evento)
+    {
+        console.log(evento)
+        if(evento.keyCode === 32)
+        {
+            if(this.posY === sueloY)
+            {
+                this.saltando = true;
+                this.velY = this.impulso;
+                this.dinoDiv.classList.remove("dino-corriendo");
+            }
+        }
+
+    }
+
+    mover(deltaTime, sueloY)
+    {
+        this.posY += this.velY * deltaTime;
+
+        if(this.posY < sueloY)
+        {
+            this.tocarSuelo(sueloY);
+            
+        }
+        this.dinoDiv.style.bottom = this.posY + "px";
+    }
+
+    tocarSuelo(sueloY)
+    {
+        this.posY = sueloY;
+        this.velY = 0;
+        if(this.saltando)
+        {
+            this.dinoDiv.classList.add("dino-corriendo");
+
+        }
+        this.saltando = false;
+    }
+
+    estrellarse()
+    {
+        this.dinoDiv.classList.remove("dino-corriendo");
+
+    }
+}
+
+
+export class Obstaculo 
+{
+    constructor(posX, posY, obstaculoDiv)
+    {
+        this.posX = posX,
+        this.posY = posY,
+        this.obstaculoDiv = obstaculoDiv
     }
 }
